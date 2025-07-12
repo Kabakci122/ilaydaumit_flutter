a72d5174e76f03c5fc5f489beb3aae455765377a
Kabakci122 <umitkabakci05@gmail.com>
2025-07-12 13:08:03 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+import 'chat_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'İlaydaÜmit',
+      debugShowCheckedModeBanner: false,
+      home: ChatScreen(),
+    );
+  }
+}
