696331421ddd825fa35b77a104ec3e10f09fc55e
Kabakci122 <umitkabakci05@gmail.com>
2025-07-12 12:56:39 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
-
 import 'package:flutter/material.dart';
 import 'chat_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'İlaydaÜmit',
+      debugShowCheckedModeBanner: false,
       home: ChatScreen(),
     );
   }
